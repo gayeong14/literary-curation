489327cc29062f9742b90543827164ca31d75143
gayeong14 <yugayeong14@gmail.com>
2026-08-05 13:43:05 +0900

    add start button to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
-  return (
-    <div>
-      <h1>문학 동아리 작품 큐레이션</h1>
-      <p>당신은 어떤 이야기에 끌리나요?</p>
-    </div>
-  );
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="home-container">
+
+            <h1>
+                문학 동아리 작품 큐레이션
+            </h1>
+
+            <p>
+                당신은 어떤 이야기에 끌리나요?
+            </p>
+
+            <button
+                onClick={() => navigate("/test")}
+            >
+                시작하기
+            </button>
+
+        </div>
+    );
 }
 
 export default Home;
